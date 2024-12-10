f47ac27b695a7c6cc3680d46590627e03613d17b
Tahir Shaikh <altahirshaikh786@gmail.com>
2024-12-10 21:48:57 +0530

    Created Video Player Now Started Work on More Info Page

--- backend/Data/MoviesData.js
+++ backend/Data/MoviesData.js
@@ -8,7 +8,7 @@ export const MoviesData = [
     language: "English",
     year: 2002,
     time: 2,
-    video: "https://www.youtube.com/watch?v=Pq2PKu-6-co",
+    video: "https://imdb-video.media-imdb.com/vi1376109081/1434659607842-pgv4ql-1659730035326.mp4?Expires=1733925585&Signature=YZqvrI36L0EhVGli191opg0ANa91tKP0WScx9Y1J1bIwqlZRy6upiYGyA4cnF0rjwm60cnGQnsisX77ysBKOH~93946JIETyV84P66Ohff~Di-8Yh-pVdP2ca-tZozi0Fl7QUaB7Mivoqn8hFQIMixA0ANy8wJyszr-kVZa4pViW6wteZeKflBrSGBhc09KPmA4wv-Ekx0ATK855jABrnWPDlMGC6W-6taxBhMWNZkPuZ5w~vg~NrUaK7sCwQ51drJMpDZfueJzDbpBqxgAurUh9HnY7bxAxvRyoppA0MZl6wGsHabi4K55LxPE7Qv9X3GWsM93bC4LqNVBNtIE1ng__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
     rate: 4,
   },
   {
@@ -20,7 +20,7 @@ export const MoviesData = [
     language: "English",
     year: 2008,
     time: 2.3,
-    video: "https://www.youtube.com/watch?v=EXeTwQWrcwY",
+    video: "https://imdb-video.media-imdb.com/vi324468761/1434659607842-pgv4ql-1563712918369.mp4?Expires=1733925853&Signature=KoOXI8xMGnUj9sIE3-SXhj6~hdT8qvNK1~GZ9RYUhjiMxXuU-ivY3hr-u3ZblpFIiUEYllUN~n5vzQBKQ1wPoEz2jDgm4Nd8iIo9Scc6TRWeBB2mdikojQ5iHi8c2NyuZkCi2D9kpHfEpFNIz2vFGxjD1KndkoXRqiFgewjeAPjS5yogDHA7Vtz24FZfkZFzecLevPWGBGhfN9BBfhtQG8BKMGKp5qDR~WBOCpB4qys2YlFRHtia8PvhG7DJyCdp5k92lD8-vLvE1EjDKU8i1yOGq5pw6CtdNpd3DYerd5JGvwL1d179YAGWEtZ-cVN~uGuQ95LEh7nJkuB0-DU2~w__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
     rate: 5,
   },
   {
@@ -32,7 +32,7 @@ export const MoviesData = [
     language: "English",
     year: 2010,
     time: 2.2,
-    video: "https://www.youtube.com/watch?v=YoHD9XEInc0",
+    video: "https://imdb-video.media-imdb.com/vi2959588889/1434659607842-pgv4ql-1596404706743.mp4?Expires=1733925384&Signature=YtYcergRlByGp4Dqf0VEfobVsNBuOFE-qt8UwXEIQjTvJqRqLB5kyW9FBpGF7u4R7ts7xorqeJMvpdsLhZnxpIhfF-qTwQe~h1xPeQ1zH779Wo3ak4U-84S88liakg55tAvbqMv01tnB5IBWUGVKx9QkbZrEU7iyG24VPhqTjCOXGqU0-XmvOh8VaOuYw74~nRPrd4mVzDJTZ~L8XvkQRrMSDwbAgIPR-o-haN3QoyuTA~zIYyG4Wal-8Bzs6UZiVSFrcmg49mW3MVweV58XUYQoR2BScnvgxcXXy2tKcjtd7DQ60eTW4g7Ywf4uEZMrlrqPdU0faA6xm1mHjDrdmw__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
     rate: 4,
   },
   {
@@ -44,7 +44,7 @@ export const MoviesData = [
     language: "English",
     year: 2014,
     time: 2.4,
-    video: "https://www.youtube.com/watch?v=zSWdZVtXT7E",
+    video: "https://imdb-video.media-imdb.com/vi1586278169/1434659607842-pgv4ql-1616202363366.mp4?Expires=1733925425&Signature=fE8FbUwsrKkqEpMzcvcTE1bgfbMsLLsOKLjQ45NJreUO5ucOxMzlcSp-aqcI4qml2hyfKKP0M-FKesZYKZvP7Y-4oL6SFf~q-4wxn0rQnq8gQv~CA9N9IuRakohqSO9QOun1gEoWwFeXmiPd7lmx0I8tH625NL5mWRm1NpaiWTnt3gbLYnxHK24ghlclTIhZ~t4ORxaQpOcWrVLgi-mBnvfDPooxv95De9DVWqQO6NyJV9Ot4350mFw4x-dUnwb5MiNwoK60g6k-fdAdpTJG8pyTy5KvDZ3oYfUp7NibQPRBpZCaej1dc60vgDWMR-iPHyLpCm4bK-bLzm4DXI7RhQ__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
     rate: 4,
   },
   {
@@ -56,7 +56,7 @@ export const MoviesData = [
     language: "English",
     year: 1999,
     time: 2.1,
-    video: "https://www.youtube.com/watch?v=m8e-FF8MsqU",
+    video: "https://imdb-video.media-imdb.com/vi3203793177/1434659607842-pgv4ql-1563703449133.mp4?Expires=1733925748&Signature=uuQWYPheXP-doGr~FWADNfiHW27Y6Ux367b-WvAwCzuDSImlmT1duRbdWZLPon2aB9tV6lzNrHV2SukH83AStcCf42wWAokPGRx~bSqpUF9fpsnCCDvzIkvfw8N5Ff-o2HyyZCGULAeDUgMCfrGLM96aRPWnZaLQHK1wAayMxjzFc8EJCa9uzwHUNj7q3zY1Q7mvqZQINKLDjvxfJYBMNk6xYzxUaZGFyaihOI3vDwCTOzTPpnRXNqVFz6wRixKcoT9luZosKYEpx0kRCpcwLzc-Nq5ZBJqgppJQnG1V4H1eoiRZHwC5HWkEItzffznFYtfBGFg2ju9LtF~3Du1s6g__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
     rate: 4,
   },
   {
@@ -68,7 +68,8 @@ export const MoviesData = [
     language: "English",
     year: 2000,
     time: 2.3,
-    video: "https://www.youtube.com/watch?v=owK1qxDselE",
+    video:
+      "https://imdb-video.media-imdb.com/vi1202323737/1434659607842-pgv4ql-1563695975562.mp4?Expires=1733925534&Signature=XKyz1ZHfk7MQZuA~wWpgb4UnP357PiE6iTrgu7ZviShVZnv-dXGydepuFJ7ih5AgUFCoeiay0QLn8qkNgPwfpS2~Kwgmn-lrN4gyGaLaXbQeiSPeER4SwjtgsVDAj8x5qlOArXUOEo7vzXkOuq1JoWN9fWBHIsCy7gEJGaKb4kMoD~0NEltOT2VAPm0Eu~hgPUWbc3eKrirxfukNyRBAwwfZUIxrb5M802KNBdnwx1RgMY3pVAO3qgghBSXjRyKR7uTbyMbKOHloLrmhuVbl~WiXmISMPPf5Gvk00TzFp7y~vijvMlHV-QuYmsZ0lC~yUBEAhsD4d1-lRE9hV~ehzg__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
     rate: 4,
   },
   {
@@ -80,7 +81,7 @@ export const MoviesData = [
     language: "English",
     year: 1994,
     time: 2.3,
-    video: "https://www.youtube.com/watch?v=s7EdQ4FqbhY",
+    video: "https://imdb-video.media-imdb.com/vi2620371481/1434659607842-pgv4ql-1450205469045.mp4?Expires=1733925835&Signature=srsbRcCB7I~AN6mF~j11IQvG2oqyfkwg8GF0BwcCrb5-~HYUaeE1BLLXCkJBRZC9PsZRzoksU0waQg36AEDYsOvKGu12LCEd5O4U8ciXFzNYazHHsGrV3kyePp00tSriz-ypSXgT~bAKEAt1DH7NgdP9BilIB90JT2sKH4JnH2JOhib~BAu2wYWrY8ogOhLh7aenhNbazNFoJXnyr2lsUFVS8T8jzTj2osVFeldg8C-Gi~yBoz3Glm46pDtYgHpPMUkZ3l1CXk0B-tHfGjH4pIOJqtsgVfWk3AiHY6DQhl~H07QqJBbtoUYQUgYjzVM3GNZFN7nh~3PoXFQZogja2Q__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
     rate: 4,
   },
   {
@@ -92,7 +93,7 @@ export const MoviesData = [
     language: "English",
     year: 1972,
     time: 2.5,
-    video: "https://www.youtube.com/watch?v=sY1S34973zA",
+    video: "https://imdb-video.media-imdb.com/vi1348706585/1434659607842-pgv4ql-1616202346191.mp4?Expires=1733926043&Signature=U~qpg-o87bSpldECARUDWys9DPLOK4tzmzmkfe4Q7n2uabYDL4ZlGmxb0Vp4cfEc8869IzQ1YqRDEsHUh9BbHZh6UiAHBVb0nT23wYTROKq7iiMhJyi9TufSoEwA3TTsasBf21B1aDaPb9jWAUep0lMsLSXZRjbnI8GX60yGWqyrQVmkcyfu~dakoFI2MWIuDt77IU8eeCFjv8BZzLBaYKqHtqi5bjNVpj1No1w38Tq~WvDRDDtFVVZu1df8UtOvTLqCOxQx~3XzAYstO36gfsLe5hLv6tYYQzr4CZCZByyeMpsXReGI9ZudOoMkfZucgei76xoOjfem872x6WKhjQ__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
     rate: 5,
   },
   {
@@ -104,7 +105,7 @@ export const MoviesData = [
     language: "English",
     year: 1999,
     time: 2.1,
-    video: "https://www.youtube.com/watch?v=SUXWAEX2jlg",
+    video: "https://imdb-video.media-imdb.com/vi123455001/1434659607842-pgv4ql-1727104628503.mp4?Expires=1733925570&Signature=cpQmTUQ1xKFsUsDQP0kzxAoZ5a8s~LZdZA~QeGD5joLgFV4zAeCrH3jljbvXB9LUjlAjMe0lZVps5R~VSQWv7qzHcOEHbn8Uxr~r74zLJ0aGaefJCBR-XTe4TKrvzy20X~J1yStelXylc7qhYIe5kRc~yVTsc2oH9RoKidc5A1al--7j4wCRjZA5TF6f1HIXNlZJJ3pE-5G5eQryWhBXBL7BpDHqBZzmB3~JVtfhABDMgQ6ms29iTQIxH-tcomhlQxJsu3bXqoAyVIjitW7Vw3fTHX4quNiQNETUO6MTy~3aywGvDstYk2NejJVHwoZ1lI6RzY4rvHtg24JISNHMSA__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
     rate: 4,
   },
   {
@@ -116,7 +117,7 @@ export const MoviesData = [
     language: "English",
     year: 1994,
     time: 2.2,
-    video: "https://www.youtube.com/watch?v=bLvqoHBptjg",
+    video: "https://imdb-video.media-imdb.com/vi3567517977/1434659607842-pgv4ql-1564505606861.mp4?Expires=1733925483&Signature=UVsdKGTi-a-eThN~BVaeQWZ6Jd03WsnFXsVhkfU2o2w0WLrTxhZ7Kmi75Sd1wgMyRZHpsFtN2o994eqxF-kbLT8a8bxgY9SSG-twtfvew47ZWBHrQCzkxTGV~1prNvphgfqIKZTS5y-4UH2EoWjspvCKCurnBP66jUN66B85408Hct9r65JrCTLVdArdhQh1L8z-o7kzko587OjH0xKaXNnQDHwsM2GNd0EN66XbiECcTK3VdjHmo3CX-39-n5K1J3LB3~WSX-kLn0Hc1I1e~pxaB5h~u13MldC87Pr3boci2HYlql~A1iIqNhvE8w41yts6wzCvBBWcFrNXt6cPww__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
     rate: 4,
   },
   {
@@ -128,7 +129,7 @@ export const MoviesData = [
     language: "English",
     year: 2019,
     time: 3,
-    video: "https://www.youtube.com/watch?v=TcMBFSGVi1c",
+    video: "https://imdb-video.media-imdb.com/vi2163260441/1434659607842-pgv4ql-1556354704370.mp4?Expires=1733925518&Signature=EYKgXqQ-lm4LEQ3weWVnMysPPHiubJ98OgBsF3ESzgYPDD1eZbEZ4CaYq2qcuO5i4C31zfNbKPGBm0DB4luoLCNKlWQyvJ6z77Hpu0Rijmppu~EVL0qUs3KFAiQMUMNyff51fXCMT0~Y3e6bm~hcLJz~qF1~--99ai8U4H3Dx3~L0zRtzP4kELpD7Qx7fyO8u2yrIWUE2XvGiZO~CzAXaEaoNwN6lVHu7jar45kZdNKSr8B6wdRTKiizvzp5SV877Skk2NWoaS0KPL3TH7CUhLVzhA~ked1qzrRwE6P5rAyC29fB-Kn2fV9DsRaJCB610UN0qJf8Un~FJ81RNL25-A__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
     rate: 5,
   },
   {
@@ -140,7 +141,7 @@ export const MoviesData = [
     language: "English",
     year: 2019,
     time: 2.2,
-    video: "https://www.youtube.com/watch?v=zAGVQLHvwOY",
+    video: "https://imdb-video.media-imdb.com/vi1723318041/1434659607842-pgv4ql-1567008550851.mp4?Expires=1733925338&Signature=Ba1xGnfCZDeqTrFSyH-verB5PZYWdTrzrtmoP3GSJci5RBg0nZFlM3p397fCIhunId-63pzHjyss9mBEi~3A41eoKXsNSE9TF9i7M9gjcdl-cmgif6z9JTHj8TBtujwzZjn5XSUXY7mpl6HHk4ePrBPcWO83NVX6d3SuPCheoS59p6GBLQnTQzdcBGorG6Ic17-OA7xhjrjzybd-lsQXiY6rLBF1tB9h47C8UYe67heCOKVOhGB4wfx1XgSwSkPLJJxd09AAhR6M~aHqmVNIQ7BNTMcEEy572k54S-EmlTYWWjIaQdTtCDjzzJ8-sCvnVR0UIReQAY1tss5Pt-yM8g__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
     rate: 5,
   },
   {
@@ -152,7 +153,7 @@ export const MoviesData = [
     language: "Korean",
     year: 2019,
     time: 2.1,
-    video: "https://www.youtube.com/watch?v=5xH0HfJHsaY",
+    video: "https://imdb-video.media-imdb.com/vi1015463705/1434659607842-pgv4ql-1606954080073.mp4?Expires=1733925930&Signature=iZrmbf0wiX~A8NOdrxRRzHKV0Z2KZBovwz9hnbToNWLwQ6YQ6pRpDHk76kH1V9tImSjCeHaRCJEDFRZNxh4dJYlTCOcthJYqVsdZ~44U7iWuCFJGME-kuf0Qnu0qsgwok-Uzx2QozLqmyNREy6YgQoo~8ZcaPYqGmPu2Q9rPMU9RwEDoN62mXrTZhRpfAryW0RCbgQhRp3SsXr9i5Nu9Hn8cOZJvmuaFA--vwLkBNzp-PFCwl5mqNU58GZ7XU-lik7~0vhsV~U8IQ6B1AH7WmehJ-Ko4jpLAF3EogAclv7rqM2FbINqgkG2SSR~G50JlxN5AI9Q1pWpyEwmWaNoP4A__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
     rate: 5,
   },
   {
@@ -164,7 +165,7 @@ export const MoviesData = [
     language: "English",
     year: 2021,
     time: 2.4,
-    video: "https://www.youtube.com/watch?v=n9xhJrPXop4", 
+    video: "https://imdb-video.media-imdb.com/vi3834759449/1434659607842-pgv4ql-1731517566157.mp4?Expires=1733925939&Signature=is~HyMJpHYxT3gghjz9~sPQOVEvUjVdlPW7d18JWFrtRc94GZt0ZPLw15i3DFuEBoGBXHkxAjIoF~5wlbaG5cTx7fP6q8Xca7fUqZK48MqSJA5ulJ2JisVtkH-Tfk5m3hyDWLVGhXjNOoCXxZeqyTbjs6oIWAD6mL2Zkf7cbfTeI66zinFciDy6PfZ~NFIuSMnQLEQOqeV15ylZAG2rEb4Yxp~waQDFc9fEyWSDAW50-4-fgSwGrWPSkG78UzC-WSgvceMJaWjY~FHaLHujc6FnLoIDhAjJnp0e01z1Wl4XUjytQ6gcTHEWkAGoV2LZU7ucF0O6G-rvIGiQ-iMqZgQ__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
     rate: 4,
   },
   {
@@ -176,7 +177,7 @@ export const MoviesData = [
     language: "English",
     year: 1994,
     time: 2.2,
-    video: "https://www.youtube.com/watch?v=6hB3S9bIaco",
+    video: "https://imdb-video.media-imdb.com/vi3877612057/1434659607842-pgv4ql-1616202333253.mp4?Expires=1733925733&Signature=ooFpYbay6krduQ5P0EqzLhVj~4US-Qy9KENncTdjwT~RtJ30hzCDWyJSlhtWlu7B9t-P3oC8nN5Kv3qAynL6EU34yhnn38Y3fXN8FSzCTZpQ8yccv-frP1yTp~p8tUSATqlEBLw5EPRq4D0JT26w8WyPTZ2TrVEkC~ppPMXAsOsHjbjRDw5YNZgWeVOuDZz9DboT9fj~bA4YpAJ5eKbgDs96pmxdWv3xOmbVnEj7KivHUJWNup-NHA6AVczuo8mJN7cnRA68BO6l7eYLExpneqQYqrHbIME7HAQZm2GeZHuCZTplfRnmsr-l6j0ugYS8Wz8vJ8Ooshn8AsLP6JU32w__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
     rate: 5,
   },
 ];
--- frontend/src/Components/HeroVideo.jsx
+++ frontend/src/Components/HeroVideo.jsx
@@ -1,23 +1,47 @@
-import React from 'react';
-import "../assets/CSS/components.css"
-import {useAuth} from "../Service/auth.jsx"
+import React, { useState } from "react";
+import "../assets/CSS/components.css";
+import { useAuth } from "../Service/auth.jsx";
+import ReactPlayer from "react-player";
+import { Button } from "react-bootstrap";
 
 const HeroVideo = () => {
-    const { film} = useAuth();
-    
+  const [volume, setVolume] = useState(true)
+  const { film } = useAuth();
+
+  const handleSound = () => {
+    setVolume(!volume);
+  }
+
+  const handleInfo = (id) => {
+    const response = id;
+    console.log('Response', response);
+  }
+
   return (
     <>
-        {film.message && film.message.length > 0 && (
-        <div className="div">
-          <h1>{film.message[0].name}</h1>
-          <video src controls>
-            <source src={film.message[0].video} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
+      {film.message && film.message.length > 0 && (
+        <div className="video-container">
+          <ReactPlayer
+            url={film.message[0].video}
+            playing={true}
+            loop={true}
+            muted={volume}
+            controls={false}
+            width="100%"
+            height="auto"
+          />
+          <div className="video-content">
+            <h1>{film.message[0].name}</h1>
+            <p>{film.message[0].desc}</p>
+            <div className="buttons d-flex justify-content-between">
+              <Button onClick={handleInfo(film.message[0]._id)}><i className="bi bi-info-circle-fill"></i> More Info</Button>
+              <Button className="bg-dark bg-gradient" onClick={handleSound}>{volume ? <i className="bi bi-volume-mute-fill"></i>:<i class="bi bi-volume-up-fill"></i>}</Button>
+            </div>
+          </div>
         </div>
       )}
     </>
-  )
-}
+  );
+};
 
-export default HeroVideo
+export default HeroVideo;
